c9776edb10d8d9b11e1b39b9a4664d1872a435c3
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-12-11 15:43:57 +0600

    implemented mail service with nodemailer for sending emails

---
+++ server/utils/mailService.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+const mailService = async (email, title, body) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: process.env.MAIL_SECURE,
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: `Codex👻 <Your Learning Partner>`,
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+
+    console.log(info);
+    return info;
+
+    //
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = mailService;
