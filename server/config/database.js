e38bc10a564b8e6099ebb73a1b2145826b6ad287
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-12-11 13:35:07 +0600

    added UserModel Schema

--- server/config/database.js
+++ server/config/database.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URL, {});
-    console.log(`Database Connected Successfully`);
+    console.log(`Database Connected Successfully...`);
   } catch (error) {
-    console.error("Error connecting  to the Database", error.message);
+    console.error("Failed!!! Error connecting  to the Database", error.message);
     process.exit(1);
   }
 };
---
+++ server/models/UserModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: [true, "First Name required"],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last Name required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email required"],
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password required"],
+  },
+  accountType: {
+    type: String,
+    enum: ["Admin", "Student", "Instructor"],
+    required: true,
+  },
+  additionalDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Profile",
+    require: true,
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  image: {
+    type: String,
+    required: true,
+  },
+  courseProgress: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseProgress",
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
